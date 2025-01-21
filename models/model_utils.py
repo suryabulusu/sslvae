9e33a844f5718191776d747b7c24b76b8968eba6
Surya Bulusu <teja.surya59@gmail.com>
2025-01-22 02:51:13 +0530

    adds reinmax algo2

--- models/model_utils.py
+++ models/model_utils.py
@@ -39,3 +39,26 @@ def straight_through(theta, tau=1.0):
     # bwd => D = pi_1; so dD/dx = dpi_1/dx
 
     return D
+
+
+def reinmax_st(theta, tau=2.0):
+    # sample D ~ softmax(theta) fwd pass
+    pi_0 = F.softmax(theta, dim=-1)
+    D = torch.multinomial(pi_0, num_samples=1)
+    D = F.one_hot(D.squeeze(), num_classes=theta.size(-1)).float()
+
+    # bwd pass set up two new probs pi_1 and pi_2
+    pi_1 = (D + F.softmax(theta / tau, dim=-1)) / 2
+    pi_1 = F.softmax(((torch.log(pi_1) - theta).detach() + theta), dim=-1)
+    # fwd => pi_1 = F.softmax(F.log(pi_1)) = pi_1 stable
+
+    pi_2 = 2 * pi_1 - pi_0 / 2
+    D = pi_2 - pi_2.detach() + D.detach()
+    # fwd => D = pi_2 - pi_2 + D = D
+    # bwd = D = pi_2
+    # dpi_2 / dtheta = 2 * dpi_1 / dtheta - 1/2 * pi_0 / dtheta
+    # dpi_1 / dtheta = dpi_1 / dinternal * dinternal / dtheta 
+    # internal = theta due to detach.. -> dinternal / dtheta = I
+    # dpi_1 / dinternal = it is typical softmax jacobian
+
+    return D
